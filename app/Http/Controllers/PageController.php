f1852ebf0f754363d953bb84f6ed830d18531312
Febrian Steven Se <150250188+sefebri4@users.noreply.github.com>
2025-06-17 19:17:47 +0700

    Add files via upload

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Berita;
+use App\Models\Prestasi;
+use App\Models\Proyek;
+use App\Models\Informasi;
+use App\Models\Dosen;
+use App\Models\Mahasiswa;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $berita = Berita::latest()->take(3)->get();
+        $prestasi = Prestasi::latest()->take(3)->get();
+        $proyek = Proyek::latest()->take(3)->get();
+        return view('index', compact('berita', 'prestasi', 'proyek'));
+    }
+
+    public function dekan()
+    {
+        return view('dekan');
+    }
+
+    public function dosen()
+    {
+        $dosen = Dosen::all();
+        return view('dosen', compact('dosen'));
+    }
+
+    public function mahasiswa()
+    {
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa', compact('mahasiswa'));
+    }
+
+    public function prestasi()
+    {
+        $prestasi = Prestasi::latest()->paginate(6);
+        return view('prestasi', compact('prestasi'));
+    }
+
+    public function galeri()
+    {
+        return view('galeri');
+    }
+
+    public function proyek()
+    {
+        $proyek = Proyek::latest()->paginate(6);
+        return view('proyek', compact('proyek'));
+    }
+
+    public function informasi()
+    {
+        $informasi = Informasi::latest()->paginate(6);
+        return view('informasi', compact('informasi'));
+    }
+
+    public function berita()
+    {
+        $berita = Berita::orderBy('tanggal_posting', 'desc')->paginate(6);
+        return view('berita', compact('berita'));
+    }
+
+    public function beritaDetail($id)
+    {
+        $berita = Berita::findOrFail($id);
+        return view('berita_detail', compact('berita'));
+    }
+}
