7a1517b5868e593c92d058cb9fc7652fbc61b5d6
Febrian Steven Se <150250188+sefebri4@users.noreply.github.com>
2025-06-17 19:42:16 +0700

    Delete public/inc directory

--- public/inc/preloader.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Back to top button -->
-<a href="#" class="btn back-to-top"><i class="fa fa-chevron-up"></i></a>
-
-<!-- Pre Loader -->
-<div id="loader" class="show">
-    <div class="loader"></div>
-</div>
